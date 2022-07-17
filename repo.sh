12efafb49ef2e0a632785e9e94aa1d6160ec1706
Rosa Masferre <105929586+rosa-masferre@users.noreply.github.com>
2022-07-17 14:32:03 +0200

    Update repo.sh
    ahora el commit desde git hub con el post build para el job notify

--- repo.sh
+++ repo.sh
@@ -3,4 +3,5 @@ echo "prueba webhook y pipeline"
 echo "2da prueba con toda la integracion configurada"
 echo "probocaré un push"
 echo "error en el puerto del ngrok, arreglado"
+echo "ahora el commit desde git hub con el post build para el job notify"
 
